1b0be0eaba572af645bc0019ff6b699e4b485aa7
Jan Cizmar <cizmar@chlupac.com>
2021-02-06 08:05:26 +0100

    PG-45 Rename to Tolgee

--- public/index.js
+++ public/index.js
@@ -1,18 +1,25 @@
-const pg = new window["@polygloat/core"].Polygloat({
-    apiUrl: "https://app.polygloat.io",
-    apiKey: "",
+const pg = new window["@tolgee/core"].Tolgee({
+    apiUrl: "https://app.tolgee.io",
+    //apiKey: "",
     filesUrlPrefix: "/i18n/",
     defaultLanguage: "en",
     inputPrefix: "{{",
     inputSuffix: "}}",
     watch: true,
-    ui: window["@polygloat/ui"].UI,
+    ui: window["@tolgee/ui"].UI,
 });
 
 pg.run().then(() => {
     document.getElementById("loading").style.display = "none";
+    document.title = pg.instant("tolgee_example_title", undefined, true);
 })
 
 document.getElementById("languageSelect").addEventListener("change", (e) => {
     pg.lang = e.target.value;
+});
+
+pg.onLangChange.subscribe(() => {
+    pg.translate("tolgee_example_title", undefined, true).then(t => {
+        document.title = t;
+    });
 });
