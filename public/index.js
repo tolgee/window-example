6b429571bcaff32c869c4ac80779d0b5ff7f376b
Jan Cizmar <cizmar@chlupac.com>
2021-09-30 13:36:25 +0200

    feat(js sdk): Enable default value providing TG-335

--- public/index.js
+++ public/index.js
@@ -1,26 +1,41 @@
-const pg = new window["@tolgee/core"].Tolgee({
-    apiUrl: "https://app.tolgee.io",
-    //apiKey: "",
-    filesUrlPrefix: "/i18n/",
-    defaultLanguage: "en",
-    inputPrefix: "{{",
-    inputSuffix: "}}",
-    watch: true,
-    ui: window["@tolgee/ui"].UI,
+const tg = new window["@tolgee/core"].Tolgee({
+  apiUrl: "https://app.tolgee.io",
+  apiKey: "s7tkvn0q2087otkgkd4l5okdmk",
+  filesUrlPrefix: "/i18n/",
+  defaultLanguage: "en",
+  inputPrefix: "{{",
+  inputSuffix: "}}",
+  watch: true,
+  ui: window["@tolgee/ui"].UI,
 });
 
-pg.run().then(() => {
-    document.getElementById("languageSelect").value = pg.lang;
-    document.getElementById("loading").style.display = "none";
-    document.title = pg.instant("tolgee_example_title", undefined, true);
-})
+tg.run().then(() => {
+  document.getElementById("languageSelect").value = tg.lang;
+  document.getElementById("loading").style.display = "none";
+  document.title = tg.instant("tolgee_example_title", undefined, true);
+  translate();
+});
 
 document.getElementById("languageSelect").addEventListener("change", (e) => {
-    pg.lang = e.target.value;
+  tg.lang = e.target.value;
 });
 
-pg.onLangChange.subscribe(() => {
-    pg.translate("tolgee_example_title", undefined, true).then(t => {
-        document.title = t;
-    });
+const translate = () => {
+  // You can pass arguments to translate function
+  tg.translate("tolgee_example_title", undefined, true).then((t) => {
+    document.title = t;
+  });
+
+  tg.translate({
+    key: "hello",
+    defaultValue: "Hello {name} {surname}.",
+    params: { name: "John", surname: "Doe" },
+  }).then((t) => {
+    document.getElementById("target").innerText = t;
+  });
+};
+
+//refresh the texts when language is changed
+tg.onLangChange.subscribe(() => {
+  translate();
 });
